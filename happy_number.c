b104aea3122e95646aadc896e661dda05625b7b0
Balaraju9 <119840117+Balaraju9@users.noreply.github.com>
2023-01-08 18:06:35 +0530

    #include<stdio.h> int main(){     int n,i,r,s=0;     scanf("%d",&n);     while(n>0){         r=n%10;         s=s+r*r;         n=n/10;         if(n==0&&s>=10){             n=s;             s=0;         }
        }
        if(s==1){
            printf("True");
        }
        else{
            printf("False");
        }
    }

--- happy_number.c
+++ happy_number.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 int main(){
-    int n,r,s=0;
+    int n,i,r,s=0;
     scanf("%d",&n);
-    while(n!=0){
+    while(n>0){
         r=n%10;
         s=s+r*r;
-        n=n/10;
+        n=n/10;     
         if(n==0&&s>=10){
             n=s;
             s=0;
         }
+
     }
-    if(s==1) printf("True");
-    else printf("False");
+    if(s==1){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
 }

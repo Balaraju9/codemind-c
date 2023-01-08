2472a0d38f01e3dc2c514fbad1a3dd4b3bff1994
Balaraju9 <119840117+Balaraju9@users.noreply.github.com>
2023-01-08 17:46:03 +0530

    #include<stdio.h> int main(){     int n,r,m=0;     scanf("%d",&n);     while(n!=0){         r=n%10;         if(r>m){             m=r;         }         n=n/10;     }     printf("%d",m); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main(){
-    int n,sum=0,r;
+    int n,r,m=0;
     scanf("%d",&n);
     while(n!=0){
         r=n%10;
-        if(sum<r){
-            sum=r;
+        if(r>m){
+            m=r;
         }
         n=n/10;
     }
-    printf("%d",sum);
+    printf("%d",m);
 }

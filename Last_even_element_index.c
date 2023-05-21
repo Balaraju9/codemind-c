dca940f68ef9db9e8a09c36332b135571b212ae8
Balaraju9 <119840117+Balaraju9@users.noreply.github.com>
2023-05-21 10:26:59 +0530

    #include<stdio.h>
    int findLastEvenIndex(int arr[], int n) {
        int lastIndex = -1;
        for (int i = 0; i < n; i++) {
            if (arr[i] % 2 == 0) {
                lastIndex = i;
            }
        }
        return lastIndex;
    }
    
    int main() {
        int n;
    
        // Read the length of the array
        scanf("%d", &n);
    
        int arr[n];
    
        // Read the array elements
        for (int i = 0; i < n; i++) {
            scanf("%d", &arr[i]);
        }
    
        // Find the index of the last even number
        int lastEvenIndex = findLastEvenIndex(arr, n);
    
        // Display the last even number index
        printf("%d
    ", lastEvenIndex);
    
        return 0;
    }

---
+++ Last_even_element_index.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int findLastEvenIndex(int arr[], int n) {
+    int lastIndex = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            lastIndex = i;
+        }
+    }
+    return lastIndex;
+}
+
+int main() {
+    int n;
+
+    // Read the length of the array
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read the array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Find the index of the last even number
+    int lastEvenIndex = findLastEvenIndex(arr, n);
+
+    // Display the last even number index
+    printf("%d
+", lastEvenIndex);
+
+    return 0;
+}

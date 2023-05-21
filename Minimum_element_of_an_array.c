705eb1ef884dc4225a21ad8a8390545371d74aa1
Balaraju9 <119840117+Balaraju9@users.noreply.github.com>
2023-05-21 10:04:28 +0530

    #include<stdio.h>
    int findMaximum(int arr[], int n) {
        int max = arr[0];
        for (int i = 1; i < n; i++) {
            if (arr[i] < max) {
                max = arr[i];
            }
        }
        return max;
    }
    
    int main() {
        int n;
    
        // Read the length of the array
        scanf("%d", &n);
    
        int arr[n];
    
        // Read the array elements
        for (int i = 0; i < n; i++) {
            scanf("%d", &arr[i]);
        }
    
        // Find the maximum element
        int maximum = findMaximum(arr, n);
    
        // Display the maximum element
        printf("%d
    ", maximum);
    
        return 0;
    }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int findMaximum(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+
+    // Read the length of the array
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read the array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Find the maximum element
+    int maximum = findMaximum(arr, n);
+
+    // Display the maximum element
+    printf("%d
+", maximum);
+
+    return 0;
+}
